714243436703a3832f770e3793557c272c019bbf
Brent <BrentC0328@gmail.com>
2022-08-06 17:44:52 -0400

    variables simplified, X and O passing

--- src/main/java/com/github/curriculeon/Board.java
+++ src/main/java/com/github/curriculeon/Board.java
@@ -9,32 +9,13 @@ public class Board {
 
 
     private final Character[][] matrix;
-
-
     public Board(Character[][] matrix) {
         this.matrix = matrix;
 
-        Character topLeft = this.matrix[0][0];
-        Character topMiddle = this.matrix[0][1];
-        Character topRight = this.matrix[0][2];
-        Character middleLeft = this.matrix[1][0];
-        Character middleMiddle = this.matrix[1][1];
-        Character middleRight = this.matrix[1][2];
-        Character bottomLeft = this.matrix[2][0];
-        Character bottomMiddle = this.matrix[2][1];
-        Character bottomRight = this.matrix[2][2];
-
-        Character[] topRow = new Character[] {topLeft, topMiddle, topRight};
-        Character[] middleRow = new Character[] {middleLeft, middleMiddle, middleRight};
-        Character[] bottomRow = new Character[]{bottomLeft, bottomMiddle, bottomRight};
-        Character[] leftColumn = new Character[] {topLeft, middleLeft, bottomLeft};
-        Character[] middleColumn = new Character[] {middleLeft, middleMiddle, bottomMiddle};
-        Character[] rightColumn = new Character[] {topRight, middleRight, bottomRight};
-        Character[] leftDiagonal = new Character[] {topLeft, middleMiddle, bottomRight};
-        Character[] rightDiagonal = new Character[] {topRight, middleMiddle, bottomLeft};
     }
 
-    public Boolean isInFavorOfX() {
+    public Boolean isInFavorOf(Character character) {
+
         Character topLeft = this.matrix[0][0];
         Character topMiddle = this.matrix[0][1];
         Character topRight = this.matrix[0][2];
@@ -49,54 +30,43 @@ public class Board {
         Character[] middleRow = new Character[] {middleLeft, middleMiddle, middleRight};
         Character[] bottomRow = new Character[]{bottomLeft, bottomMiddle, bottomRight};
         Character[] leftColumn = new Character[] {topLeft, middleLeft, bottomLeft};
-        Character[] middleColumn = new Character[] {middleLeft, middleMiddle, bottomMiddle};
+        Character[] middleColumn = new Character[] {topMiddle, middleMiddle, bottomMiddle};
         Character[] rightColumn = new Character[] {topRight, middleRight, bottomRight};
         Character[] leftDiagonal = new Character[] {topLeft, middleMiddle, bottomRight};
         Character[] rightDiagonal = new Character[] {topRight, middleMiddle, bottomLeft};
-        if (hasSameElement('X', topRow)){
+
+        if (hasSameElement(character, topRow)){
+            return true;}
+
+        if (hasSameElement(character, middleRow)){
+            return true;}
+
+        if (hasSameElement(character, bottomRow)){
+            return true;}
+
+        if (hasSameElement(character, leftColumn)){
+            return true;}
+
+        if (hasSameElement(character, middleColumn)){
+            return true;}
+
+        if (hasSameElement(character, rightColumn)){
+            return true;}
+
+        if (hasSameElement(character, leftDiagonal)){
+            return true;}
+
+        if (hasSameElement(character, rightDiagonal)){
             return true;}
         return false;
+    }
+    public Boolean isInFavorOfX() {
+        return isInFavorOf('X');
     }
 
     public Boolean isInFavorOfO() {
-        Character topLeft = this.matrix[0][0];
-        Character topMiddle = this.matrix[0][1];
-        Character topRight = this.matrix[0][2];
-        Character middleLeft = this.matrix[1][0];
-        Character middleMiddle = this.matrix[1][1];
-        Character middleRight = this.matrix[1][2];
-        Character bottomLeft = this.matrix[2][0];
-        Character bottomMiddle = this.matrix[2][1];
-        Character bottomRight = this.matrix[2][2];
 
-        Character[] topRow = new Character[] {topLeft, topMiddle, topRight};
-        Character[] middleRow = new Character[] {middleLeft, middleMiddle, middleRight};
-        Character[] bottomRow = new Character[]{bottomLeft, bottomMiddle, bottomRight};
-        Character[] leftColumn = new Character[] {topLeft, middleLeft, bottomLeft};
-        Character[] middleColumn = new Character[] {middleLeft, middleMiddle, bottomMiddle};
-        Character[] rightColumn = new Character[] {topRight, middleRight, bottomRight};
-        Character[] leftDiagonal = new Character[] {topLeft, middleMiddle, bottomRight};
-        Character[] rightDiagonal = new Character[] {topRight, middleMiddle, bottomLeft};
-
-        if (hasSameElement('O', topRow)){
-            return true;}
-
-        if (hasSameElement('O', middleRow)){
-            return true;}
-
-        if (hasSameElement('O', bottomRow)){
-            return true;}
-
-        if (hasSameElement('O', leftColumn)){
-            return true;}
-
-        if (hasSameElement('O', middleColumn)){
-            return true;}
-
-        if (hasSameElement('O', rightColumn)){
-            return true;}
-        return false;
-
+        return isInFavorOf('O');
     }
 
     public Boolean isTie() {
